b3bcec927593dccb248302277a2d803e354760f8
Max Thayer <garbados@gmail.com>
2013-09-21 16:14:24 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,48 @@
+var select = require('soupselect').select,
+    htmlparser = require('htmlparser'),
+    request = require('request'),
+    schema_url = 'http://schema.org';
+
+function parse_html (html, cb) {
+  var handler = new htmlparser.DefaultHandler(cb),
+      parser = new htmlparser.Parser(handler);
+
+  return parser.parseComplete(html);
+}
+
+/*
+ * GET home page.
+ */
+
+exports.index = function (req, res) {
+  res.render('index', { title: 'Express' });
+};
+
+exports.entity = function (req, res) {
+  request.get([schema_url, req.params.entity].join('/'), function (err, response) {
+    if (err) {
+      res.send(400, err);
+    } else {
+      parse_html(response.body, function (error, dom) {
+        if (error) {
+          res.send(400, error);
+        } else {
+          var rows = select(dom, 'tbody tr')
+              .map(function (row) {
+                var property = select(row, 'th.prop-nam code a')[0];
+                var type = select(row, '.prop-ect')[0]
+                if (property && type) {
+                  return {
+                    url: schema_url + property.attribs.href,
+                    property: property.children[0].raw,
+                    type: type.children[0].raw.replace(/\s+/g, ''),
+                    description: select(row, 'td.prop-desc')[0].children[0].raw
+                  }; 
+                }
+              });
+          res.json(rows);
+        }
+      });
+    }
+  });
+}
